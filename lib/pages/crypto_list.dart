d36238084e2cefc77086b5c505cf6dbb21938092
LeGaCy2096 <LeGaCy.20.08.96@gmail.com>
2021-11-23 22:09:21 +0200

    Сделан дизайн кнопок, заготовка для вывода списка валют

--- lib/pages/crypto_list.dart
+++ lib/pages/crypto_list.dart
@@ -1,22 +1,72 @@
-import 'package:flutter/cupertino.dart';
+import 'dart:collection';
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class CryptoList extends StatelessWidget {
+  var bitcoin = {
+    'name': 'Bitcoin',
+    'shortName': 'BTC',
+    'price': 20000,
+    'dynamic': -2.71
+  };
+  var etherium = {
+    'name': 'Etherium',
+    'shortName': 'ETH',
+    'price': 1230,
+    'dynamic': 3.00
+  };
+  var binance = {
+    'name': 'Binance',
+    'shortName': 'BNB',
+    'price': 20.01,
+    'dynamic': 0.02
+  };
+
+  List currencies = [];
+
+  CryptoList() {
+    for (int i = 1; i <= 3; i++) {
+      currencies.add(bitcoin);
+      currencies.add(etherium);
+      currencies.add(binance);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
+        const Padding(padding: EdgeInsets.only(top: 30)),
         Container(
-          color: Colors.red,
+          decoration: const BoxDecoration(
+              borderRadius: BorderRadius.all(Radius.circular(5)),
+              color: Color.fromARGB(255, 25, 27, 31)),
           child: Row(
             mainAxisSize: MainAxisSize.min,
             children: [
-              TextButton(onPressed: () {}, child: Text('Swap')),
-              TextButton(onPressed: () {}, child: Text('Charts'))
+              const Padding(padding: EdgeInsets.all(3)),
+              TextButton(
+                  onPressed: () {},
+                  child: const Text(
+                    'Swap',
+                    style: TextStyle(color: Colors.grey),
+                  )),
+              TextButton(
+                onPressed: () {},
+                child: const Text(
+                  'Charts',
+                  style: TextStyle(color: Colors.white),
+                ),
+                style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(
+                        const Color.fromARGB(255, 33, 36, 41))),
+              ),
+              const Padding(padding: EdgeInsets.all(3)),
             ],
           ),
         ),
+        const Padding(padding: EdgeInsets.only(top: 30)),
       ],
     );
   }
